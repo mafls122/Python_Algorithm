3a9455b646753c36904827e8f9644cc027a1da64
JUA <mafls122@naver.com>
2021-10-21 01:46:33 +0900

    ADD

---
+++ 01_Stack/1_stack.py
@@ -0,0 +1,22 @@
+stack_list = []
+
+stack_list.append(1)
+stack_list.appnd(2)
+stack_list.append(3)
+stack_list.pop()
+stack_list.append(4)
+stack_list.append(5)
+stack_list.append(6)
+stack_list.pop()
+
+print(stack_list)
+
+'''
+stack 은
+append() 후입
+pop() 선출
+을 통해 쉽게 구현 가능
+'''
+
+
+
---
+++ 01_Stack/Stack_README.md
@@ -0,0 +1,2 @@
+# Python_Algorithm
+파이썬 알고리즘 정리
